b1f567694c31f140a5c2a2ed30045328f9999709
John Blaine <jablaine@uw.edu>
2018-06-15 13:38:02 -0600

    Set up express, create root server response

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('I love express');
+})
+
+app.listen(1337, () => console.log('Listening on port 1337!'));
