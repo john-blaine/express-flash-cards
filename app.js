a91b40d58f9881240329ea978bbf966eab667c45
John Blaine <jablaine@uw.edu>
2018-06-17 07:16:22 -0600

    Add /javascript get route, add html tags

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('I love express');
+  res.send('<h1>I love express</h1>');
 })
+
+app.get('/javascript', (req, res) => {
+  res.send('<h2>I also love cornbread</h2>')
+});
 
 app.listen(1337, () => console.log('Listening on port 1337!'));
