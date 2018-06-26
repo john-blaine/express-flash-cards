6314237ae51a420d34b3886c1bf9d1027a87c86a
John Blaine <jablaine@uw.edu>
2018-06-25 20:19:02 -0600

    Create card template and render on /cards url

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
-app.get('/javascript', (req, res) => {
-  res.send('<h2>I also love cornbread</h2>')
+app.get('/cards', (req, res) => {
+  res.render('card', { prompt: "Who is buried in Grant's Tomb?" })
 });
 
 app.listen(1337, () => console.log('Listening on port 1337!'));
