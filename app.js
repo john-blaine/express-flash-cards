ff5ca20509d11b0876838d262c95f9fb959acc81
John Blaine <jablaine@uw.edu>
2018-06-25 20:06:58 -0600

    Render index.pug and add additional mock data

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('<h1>I love express</h1>');
+  res.render('index');
 })
 
 app.get('/javascript', (req, res) => {
